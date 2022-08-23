25d1531145362446c50055ba359339d7d4e1a53e
Fily <filiberto@theapollogroup.com>
2022-08-23 10:59:34 -0400

    First Commit!!

---
+++ ApplicationCore/Exceptions/WeatherNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Exceptions
+{
+    public class WeatherNotFoundException: Exception
+    {
+        public WeatherNotFoundException(int basketId) : base($"No weather found with id {basketId}")
+        {
+        }
+    }
+}
---
+++ Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            // TODO: Wire this up to actual email sending logic via SendGrid, local SMTP, etc.
+            return Task.CompletedTask;
+        }
+    }
+}
